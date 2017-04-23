ecc5e3287a3164dd2b2785f6912e2289fae22be8
Gerard Casas Saez <casassg@gmail.com>
2017-04-23 11:41:33 -0600

    hackcu ready

--- new-table-assigner.py
+++ new-table-assigner.py
@@ -6,26 +6,25 @@ expo_list = ["1"]
 #get submissions from csv
 title_col = "Submission Title"
 description_col = "Plain Description"
-sponsors_row = "Desired Prizes"
+sponsors_row = "Opt-in prize"
 link_col = "Submission Url"
 
-input_path = 'data/data.csv'
-output_full_path = 'data/data_full.csv'
+input_path = 'data/devpost.csv'
+output_full_path = 'data/data.csv'
 output_gavel_path = 'data/data_gavel.csv'
 
-gavel = []
+
 full  = []
 
-with open(input_path, newline='') as csvfile:
-    submissions_reader = csv.DictReader(csvfile)
+with open(input_path) as csvfile:
+    submissions_reader = list(csv.DictReader(csvfile))
+    table_list = [i for i in range(1,len(submissions_reader)+1)]
     free_tables = table_list[:]
     print(table_list)
     for row in submissions_reader:
         print(row[title_col])
-        table = random.choice(free_tables) #(0, len(free_tables)-1)
+        table = free_tables[0] #(0, len(free_tables)-1)
         full.append(row)
-        gavel.append({'project': row[title_col], 'location': table, 'description':
-        row[description_col][:200]})
         # complete_list[-1]["expo"] = expo_list[expo]
         full[-1]['table'] = table
         free_tables.remove(table)
@@ -46,15 +45,4 @@ with open(output_full_path, 'w') as csvfile:
             'link': row[link_col]
             #'category': ',
         })
-
-
-# write gavel file (to paste to gavel)
-
-with open(output_gavel_path, 'w') as csvfile:
-    fieldnames = ['project', 'location', 'description']
-    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-
-    writer.writeheader()
-    for row in gavel:
-        writer.writerow(row)
 
