dcee4010d458ee3b75ed72ad2482af066134d13d
Gerard Casas Saez <casassg@gmail.com>
2018-02-25 11:16:52 -0700

    update to hackcu4

--- new-table-assigner.py
+++ new-table-assigner.py
@@ -8,6 +8,8 @@ title_col = "Submission Title"
 description_col = "Plain Description"
 sponsors_row = "Desired Prizes"
 link_col = "Submission Url"
+room_col = "What Room Are You In?"
+table_col = "What's Your Table Number?"
 
 input_path = 'data/devpost.csv'
 output_full_path = 'data/data.csv'
@@ -18,31 +20,25 @@ full  = []
 
 with open(input_path) as csvfile:
     submissions_reader = list(csv.DictReader(csvfile))
-    table_list = [i for i in range(1,len(submissions_reader)+1)]
-    free_tables = table_list[:]
-    print(table_list)
     for row in submissions_reader:
         print(row[title_col])
-        table = free_tables[0] #(0, len(free_tables)-1)
         full.append(row)
-        # complete_list[-1]["expo"] = expo_list[expo]
-        full[-1]['table'] = table
-        free_tables.remove(table)
+        
 
 # write full file (for expo website)
 
 with open(output_full_path, 'w') as csvfile:
-    fieldnames = ['table', 'project', 'sponsors', 'link']
+    fieldnames = ['room','table', 'project', 'sponsors', 'link']
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
     writer.writeheader()
     for row in full:
         writer.writerow(
         {
-            'table': row['table'],
+            'room': row[room_col],
+            'table': row[table_col],
             'project': row[title_col],
             'sponsors': row[sponsors_row],
             'link': row[link_col]
-            #'category': ',
         })
 
